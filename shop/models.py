9e7e4a5baa7277eb2e97922fb6e3b97235da03f8
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-14 15:50:32 +0500

    remake project strukture

--- shop/models.py
+++ shop/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
 
 class CartProduct(models.Model):
     user = models.ForeignKey("Customer", verbose_name="Покупатель", on_delete=models.CASCADE)
-    cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE)
+    cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE, related_name="related_products")
     product = models.ForeignKey(Product, verbose_name="Продукт", on_delete=models.CASCADE)
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
@@ -47,7 +47,7 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
     owner = models.ForeignKey("Customer", verbose_name="Владелец", on_delete=models.CASCADE)
-    products = models.ManyToManyField(CartProduct, blank=True)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name="related_cart")
     # total_products - чтоб показывать корректное количество товаров в корзине
     # 2 смартфона и 3 ноутбука - 2 продукта и 5 товаров
     total_products = models.PositiveIntegerField(default=0)
