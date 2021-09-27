e219bae91f977a2a367c2b44a5f5d6d324a15eba
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-27 15:04:41 +0500

    modify

--- web_magazine/settings.py
+++ web_magazine/settings.py
@@ -25,7 +25,6 @@ env = dotenv_values(ENV_PATH)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-(vw@8w0-y0y7^fjvvi3p*kzyx5+o_*c#0ce@z6pou*m64v3ew0"
 SECRET_KEY = env["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
