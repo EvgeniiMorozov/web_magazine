c934e2c39bebd0c527291bf21b89a9358c0f51e4
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-27 14:04:45 +0500

    add base template

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
+from shop.views import test_view
 from django.urls import path
 
 urlpatterns = [
+    path("", test_view, name='base')
     # path('admin/', admin.site.urls),
     # path('', include('shop.urls'))
 ]
