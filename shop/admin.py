2d618490d345cd030d8c52fee71c328c8605f532
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-27 13:46:13 +0500

    add LatestProducts

--- shop/admin.py
+++ shop/admin.py
@@ -5,6 +5,7 @@ from .models import *
 
 
 class NotebookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "category":
             return ModelChoiceField(Category.objects.filter(slug="notebooks"))
@@ -12,6 +13,7 @@ class NotebookAdmin(admin.ModelAdmin):
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "category":
             return ModelChoiceField(Category.objects.filter(slug="smartphones"))
