88af8fc4d56d24c3f601e7bca2c29f7ebfd0207c
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-27 15:02:33 +0500

    modify

--- web_magazine/settings.py
+++ web_magazine/settings.py
@@ -10,17 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
+from dotenv import dotenv_values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ENV_PATH = BASE_DIR / '.env'
+
+env = dotenv_values(ENV_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(vw@8w0-y0y7^fjvvi3p*kzyx5+o_*c#0ce@z6pou*m64v3ew0"
+# SECRET_KEY = "django-insecure-(vw@8w0-y0y7^fjvvi3p*kzyx5+o_*c#0ce@z6pou*m64v3ew0"
+SECRET_KEY = env["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
