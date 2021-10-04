9bb18839e0dd118a6374d57e04136560e2644e6c
Evgenii Morozov <zheka1101@yandex.ru>
2021-10-04 14:37:07 +0500

    lesson 16

--- shop/models.py
+++ shop/models.py
@@ -168,7 +168,7 @@ class CartProduct(models.Model):
     )
 
     def __str__(self):
-        return f"Продукт: {self.product.title} (для корзины)"
+        return f"Продукт: {self.content_object.title} (для корзины)"
 
 
 class Cart(models.Model):
@@ -184,6 +184,8 @@ class Cart(models.Model):
     final_price = models.DecimalField(
         max_digits=9, decimal_places=2, verbose_name="Общая цена"
     )
+    in_order = models.BooleanField(default=False)
+    for_anonymous_user = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.pk)
