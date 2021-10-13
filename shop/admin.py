f94ec074e5d1d2f9d3203789d09075a1fa3c0b2b
Evgenii Morozov <zheka1101@yandex.ru>
2021-10-13 10:02:25 +0500

    add auto-slugify

--- shop/admin.py
+++ shop/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from shop.models import Category, Customer, Cart, CartProduct, Order, Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Cart)
