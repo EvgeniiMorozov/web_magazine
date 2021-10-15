ff686b7cdf39e59878c22e37c4a6cc3caacb1d99
Evgenii Morozov <zheka1101@yandex.ru>
2021-10-15 10:24:51 +0500

    add base view and template

--- specs/admin.py
+++ specs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from specs.models import CategoryFeature, FeatureValidator, ProductFeatures
+
+
+admin.site.register(CategoryFeature)
+admin.site.register(FeatureValidator)
+admin.site.register(ProductFeatures)
