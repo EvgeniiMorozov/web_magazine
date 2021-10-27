8c5ee796692b2a918f4cf509993513f03b1baa66
Evgeniy Morozov <emorozov1101@gmail.com>
2021-10-27 12:49:54 +0500

    refactor models

--- shop/models.py
+++ shop/models.py
@@ -17,13 +17,6 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse("category_detail", kwargs={"slug": self.slug})
 
-    def get_fields_for_filter_in_template(self):
-        return (
-            ProductFeatures.objects.filter(category=self, use_in_filter=True)
-            .prefetch_related("category")
-            .value("feature_key", "feature_measure", "feature_name", "filter_type")
-        )
-
 
 class Product(models.Model):
 
@@ -37,64 +30,11 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def get_model_name(self):
-        return self.__class__.__name__.lower()
-
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"slug": self.slug})
 
-
-class ProductFeatures(models.Model):
-
-    RADIO = "radio"
-    CHECKBOX = "checkbox"
-
-    FILTER_TYPE_CHOICES = ((RADIO, "Радиокнопка"), (CHECKBOX, "Чекбокс"))
-
-    feature_key = models.CharField(max_length=100, verbose_name="Ключ характеристики")
-    feature_name = models.CharField(max_length=255, verbose_name="Наименование характеристики")
-    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE)
-    postfix_for_value = models.CharField(
-        max_length=20,
-        blank=True,
-        null=True,
-        verbose_name="Постфикс для значения",
-        help_text=f"Например, для характеристики 'Часы работы' к значению"
-        f" можно добавить постфикс 'часов', и как результат - значение '10 часов'",
-    )
-    use_in_filter = models.BooleanField(default=False, verbose_name="Использовать в фильтрации товаров в шаблоне")
-    filter_type = models.CharField(
-        max_length=20, verbose_name="Тип фильтра", default=CHECKBOX, choices=FILTER_TYPE_CHOICES
-    )
-    filter_measure = models.CharField(
-        max_length=50,
-        verbose_name="Единица измерения для фильтра",
-        help_text="Единица измерения для конкретного фильтра. "
-        "Например, 'Частота процессора (Ghz). Единицей измерения будет информация в скобках'",
-    )
-
-    def __str__(self):
-        return f"Категория - '{self.category.name}' | Характеристика - '{self.feature_name}'"
-
-
-class ProductFeaturesValidators(models.Model):
-
-    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE)
-    feature = models.ForeignKey(
-        ProductFeatures, verbose_name="Характеристика", on_delete=models.CASCADE, null=True, blank=True
-    )
-    feature_value = models.CharField(
-        max_length=255, unique=True, null=True, blank=True, verbose_name="Значение характеристики"
-    )
-
-    def __str__(self):
-        if not self.feature:
-            return f"Валидатор категории '{self.category.name}' - характеристика не выбранна"
-        return (
-            f"Валидатор категории '{self.category.name}' |"
-            f" Характеристика - '{self.feature.feature_name}' |"
-            f" Значение - '{self.feature_value}'"
-        )
+    def get_features(self):
+        return {f.feature.feature_name: " ".join([f.value, f.feature.unit or ""]) for f in self.features.all()}
 
 
 class CartProduct(models.Model):
@@ -132,15 +72,6 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"Покупатель: {self.user.first_name} {self.user.last_name}"
-
-
-class Specification(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    title = models.CharField(max_length=255, verbose_name="Название товара для характеристик")
-
-    def __str__(self):
-        return f"Характеристики для товара: {self.title}"
 
 
 class Order(models.Model):
