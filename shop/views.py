5123116a0adfc9bc5cc012ce1393287d9f836a89
Evgeniy Morozov <emorozov1101@gmail.com>
2021-10-27 12:43:57 +0500

    modify CategoryDetailView

--- shop/views.py
+++ shop/views.py
@@ -10,6 +10,7 @@ from shop.forms import OrderForm, LoginForm, RegistrationForm
 from shop.mixins import CartMixin
 from shop.models import Category, CartProduct, Customer, Product, Order
 from shop.utils import recalculate_cart
+from specs.models import ProductFeatures
 
 
 class MyQ(Q):
@@ -40,13 +41,56 @@ class ProductDetailView(CartMixin, DetailView):
 class CategoryDetailView(CartMixin, DetailView):
     model = Category
     queryset = Category.objects.all()
-    context_object_name = "categories"
+    context_object_name = "category"
     template_name = "shop/category_detail.html"
     slug_url_kwarg = "slug"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        query = self.request.GET.get("search")
+        category = self.get_object()
         context["cart"] = self.cart
+        context["categories"] = self.model.objects.all()
+
+        if not query and not self.request.GET:
+            context["category_products"] = category.product_set.all()
+            return context
+
+        if query:
+            products = category.product_set.filter(Q(title__icontains=query))
+            context["category_products"] = products
+            return context
+
+        # url_kwargs = {}
+        # for item in self.request.GET:
+        #     if len(self.request.GET.getlist(item)) > 1:
+        #         url_kwargs[item] = self.request.GET.getlist(item)
+        #     else:
+        #         url_kwargs[item] = self.request.GET.get(item)
+
+        url_kwargs = {
+            item: self.request.GET.getlist(item)
+            if len(self.request.GET.getlist(item)) > 1
+            else self.request.GET.get(item)
+            for item in self.request.GET
+        }
+
+        q_condition_queries = Q()
+
+        for key, value in url_kwargs.items():
+            if isinstance(value, list):
+                q_condition_queries.add(Q(**{"value__in": value}), Q.OR)
+            else:
+                q_condition_queries.add(Q(**{"value": value}), Q.OR)
+
+        pf = (
+            ProductFeatures.objects.filter(q_condition_queries)
+            .prefetch_related("product", "feature")
+            .values("product_id")
+        )
+        products = Product.objects.filter(id__in=[element["product_id"] for element in pf])
+        context["category_products"] = products
+
         return context
 
 
